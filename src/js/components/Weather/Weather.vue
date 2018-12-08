b68ef49403900ed92787445f9b781a7a503dbbd6
Nick Ford <nickforddesign@gmail.com>
2018-12-08 16:17:18 -0500

    connect openWeatherMaps api

--- src/js/components/Weather/Weather.vue
+++ src/js/components/Weather/Weather.vue
@@ -8,8 +8,32 @@
 <!--/////////////////////////////////////////////////////////////////////////-->
 
 <script>
+import { mapGetters } from 'vuex'
+import { request } from '@/utils'
+
+const api_root = 'https://api.openweathermap.org'
+const api_key = process.env.OPENWEATHERMAP_KEY
+
 export default {
-  name: 'weather'
+  name: 'weather',
+  data() {
+    return {
+      data: null
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'coords'
+    ])
+  },
+  mounted() {
+    this.fetchCurrentWeather()
+  },
+  methods: {
+    async fetchCurrentWeather() {
+      this.data = await request(`${api_root}/data/2.5/weather?lat=${this.coords.latitude}&lon=${this.coords.longitude}&appid=${api_key}`)
+    }
+  }
 }
 </script>
 
